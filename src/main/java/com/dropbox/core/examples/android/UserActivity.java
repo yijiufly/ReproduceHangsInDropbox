205d0721fadfc25c68ca9c90470b7a762f971485
Lian Gao <yijiufly@qq.com>
2018-04-25 18:05:38 -0700

    first commit

---
+++ src/main/java/com/dropbox/core/examples/android/UserActivity.java
@@ -0,0 +1,135 @@
+package com.dropbox.core.examples.android;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dropbox.core.android.Auth;
+import com.dropbox.core.examples.android.internal.OpenWithActivity;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.text.DateFormat;
+
+
+/**
+ * Activity that shows information about the currently logged in user
+ */
+public class UserActivity extends DropboxActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_user);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        Button loginButton = (Button)findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Auth.startOAuth2Authentication(UserActivity.this, getString(R.string.app_key));
+            }
+        });
+
+        Button filesButton = (Button)findViewById(R.id.files_button);
+        filesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(FilesActivity.getIntent(UserActivity.this, ""));
+            }
+        });
+
+        Button openWithButton = (Button)findViewById(R.id.open_with);
+        openWithButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWithIntent = new Intent(UserActivity.this, OpenWithActivity.class);
+                startActivity(openWithIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (hasToken()) {
+            findViewById(R.id.login_button).setVisibility(View.GONE);
+            findViewById(R.id.email_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.name_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.type_text).setVisibility(View.VISIBLE);
+            findViewById(R.id.files_button).setEnabled(true);
+            findViewById(R.id.open_with).setEnabled(true);
+            uploadFile("file://" + Environment.getExternalStorageDirectory().getPath() + "/file.txt");
+        } else {
+            findViewById(R.id.login_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_text).setVisibility(View.GONE);
+            findViewById(R.id.name_text).setVisibility(View.GONE);
+            findViewById(R.id.type_text).setVisibility(View.GONE);
+            findViewById(R.id.files_button).setEnabled(false);
+            findViewById(R.id.open_with).setEnabled(false);
+        }
+
+    }
+    private void uploadFile(String fileUri) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setCancelable(false);
+        dialog.setMessage("Uploading");
+        dialog.show();
+
+        Log.e("Dropbox", fileUri);
+        new UploadFileTask(this, DropboxClientFactory.getClient(), new UploadFileTask.Callback() {
+            @Override
+            public void onUploadComplete(FileMetadata result) {
+                dialog.dismiss();
+
+                String message = result.getName() + " size " + result.getSize() + " modified " +
+                        DateFormat.getDateTimeInstance().format(result.getClientModified());
+                Toast.makeText(UserActivity.this, message, Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                dialog.dismiss();
+
+                Log.e("Dropbox", "Failed to upload file.", e);
+                Toast.makeText(UserActivity.this,
+                        "An error has occurred",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }).execute(fileUri, "");
+    }
+    @Override
+    protected void loadData() {
+        new GetCurrentAccountTask(DropboxClientFactory.getClient(), new GetCurrentAccountTask.Callback() {
+            @Override
+            public void onComplete(FullAccount result) {
+                ((TextView) findViewById(R.id.email_text)).setText(result.getEmail());
+                ((TextView) findViewById(R.id.name_text)).setText(result.getName().getDisplayName());
+                ((TextView) findViewById(R.id.type_text)).setText(result.getAccountType().name());
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(getClass().getName(), "Failed to get account details.", e);
+            }
+        }).execute();
+    }
+
+}
